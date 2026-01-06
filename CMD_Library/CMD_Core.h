9b526a0b89141702bb08fd503fa058e9f40140c0
Hmild <15258619646@163.com>
2026-01-06 10:02:36 +0800

    增加接收帧队列, 优化接口

--- CMD_Library/CMD_Core.h
+++ CMD_Library/CMD_Core.h
@@ -1,18 +1,27 @@
 #ifndef __CMD_CORE_H
 #define __CMD_CORE_H
 
-/* 初次使用建议和CMD库数据手册一同使用, 带"选配"的字样, 代表可以传入 NULL */
+/* 初次使用建议和CMD库数据手册一同使用, 带"选配"字样的参数, 代表可以传入 NULL */
+
 
 /* 注意 头文件的宏修改了, 库内不会同步, 如果有特殊的容量需求, 将头文件的宏修改好后, 发给库开发者, 对库重新进行资源分配 */
 
+
+
 /* 核心层句柄类型 */
 typedef void * CMD_Core_Handle_t;
+
 
 /* 核心层句柄数量 */
 #define CMD_CORE_SERIAL_MEMPOOL_ELEMENT_COUNT		3
 
 
+/* 每个句柄的接收帧队列数量 */
+#define CMD_CORE_RECV_FRAME_QUEUE_COUNT					15
 
+
+
+/* 核心层共用状态枚举 */
 typedef enum
 {
 	CMD_CORE_OK,					// 通用的成功状态
@@ -23,18 +32,19 @@ typedef enum
 }CMD_Core_State_t;
 
 
+
+/* 核心层参数类型 */
 typedef struct
 {
-	char *recv_buf;						// DMA 目的缓冲区首地址指针
-	char *recv_line_buf;			// DMA 环形缓冲区首地址指针
+	char *recv_buf;										// DMA 目的缓冲区首地址指针
+	char *recv_line_buf;							// DMA 环形缓冲区首地址指针
 
-	uint16_t recv_buf_size;			// DMA 搬运目的缓冲区, 环形缓冲区长度(这俩个缓冲区大小需一致)
-	uint32_t scan_threshold;		// 串行口接收超时扫描阈值
-	uint32_t timeout_threshold;	// 串行口接收超时阈值
+	uint16_t recv_buf_size;						// DMA 搬运目的缓冲区, 环形缓冲区长度(这俩个缓冲区大小需一致)
+	uint32_t scan_threshold;					// 串行口接收超时扫描阈值
+	uint32_t timeout_threshold;				// 串行口接收超时阈值
+	uint16_t (*dma_count_get)(void); 	// DMA 通道传输计数器获取
 
-	uint16_t (*dma_count_get)(void); // DMA 通道传输计数器获取
-
-}CMD_Core_SerialParam_t;
+}CMD_Core_Param_t;
 
 
 
@@ -42,10 +52,11 @@ typedef struct
   * @brief  核心层句柄创建
   * @note   只有创建句柄跟删除句柄需要传入二级指针, 其余接口函数可直接使用句柄操作
   * @param  handle: CMD_Core_Handle_t 类型的指针
-  * @param  cfg:		CMD_Core_SerialParam_t 类型的指针
+  * @param  cfg:		CMD_Core_Param_t 类型的指针
   * @retval CMD_Core_State_t: CMD_CORE_OK, CMD_CORE_ERR_PARAM, CMD_CORE_STATE_FULL
   */
-CMD_Core_State_t CMD_Core_SerialHandle_Create(CMD_Core_Handle_t *handle, CMD_Core_SerialParam_t *cfg);
+CMD_Core_State_t CMD_Core_SerialHandle_Create(CMD_Core_Handle_t *handle, CMD_Core_Param_t *cfg);
+
 
 
 /**
@@ -62,7 +73,7 @@ CMD_Core_State_t CMD_Core_SerialHandle_Delete(CMD_Core_Handle_t *handle);
   * @brief  串行口接收超时任务
   * @note   建议放入1ms执行1次的任务区
   * @param  handle: CMD_Core_Handle_t 类型的指针
-  * @retval CMD_Core_State_t: CMD_CORE_OK, CMD_CORE_ERR_PARAM
+  * @retval CMD_Core_State_t: CMD_CORE_OK, CMD_CORE_ERR_PARAM, CMD_CORE_STATE_FULL(接收帧队列满会返回)
   */
 CMD_Core_State_t CMD_Core_SerialRecvTimeout_Task(CMD_Core_Handle_t handle);
 
--- CMD_Library/CMD_Network.h
+++ CMD_Library/CMD_Network.h
@@ -1,21 +1,27 @@
 #ifndef __CMD_NETWORK_H
 #define __CMD_NETWORK_H
 
-/* 初次使用建议和CMD库数据手册一同使用, 带"选配"的字样, 代表可以传入 NULL */
+/* 初次使用建议和CMD库数据手册一同使用, 带"选配"字样的参数, 代表可以传入 NULL */
+
 
 /* 注意 头文件的宏修改了, 库内不会同步, 如果有特殊的容量需求, 将头文件的宏修改好后, 发给库开发者, 对库重新进行资源分配 */
+
+
 
 /* 网络层句柄类型 */
 typedef void * CMD_Network_Handle_t;
 
+
 /* 网络层句柄数量 */
 #define CMD_NETWORK_MEMPOOL_ELEMENT_COUNT					3
+
 
 /* 每个句柄最大支持多少条指令 */
 #define CMD_NETWORK_MENU_MEMPOOL_ELEMENT_COUNT		40
 
 
 
+/* 网络层共用状态枚举 */
 typedef enum
 {
 	CMD_NETWORK_OK = 0,			// 通用的成功状态
@@ -30,15 +36,17 @@ typedef enum
 }CMD_Network_State_t;
 
 
+
+/* 网络层参数类型 */
 typedef struct
 {
 	CMD_Core_Handle_t	core_handle;	// 网络层使用到的核心层句柄
 
-	uint32_t speed_threshold;		// 联网速度阈值, 单位是 CMD_Network_Task() 周期
-	uint32_t timeout_threshold;	// 模块卡死阈值, 单位是 CMD_Network_Task() 周期
-	uint8_t retry_threshold;		// 重试计数器阈值, 单位是 CMD_Network_Task() 周期
+	uint32_t speed_threshold;				// 联网速度阈值, 单位是 CMD_Network_Task() 周期
+	uint8_t retry_threshold;				// 重试计数器阈值, 指令的重发次数
 
 }CMD_Network_Param_t;
+
 
 
 /**
@@ -62,40 +70,44 @@ CMD_Network_State_t CMD_Network_Handle_Delete(CMD_Network_Handle_t *handle);
 
 
 
-
 /**
   * @brief  联网状态机任务
-  * @note   未连接(离线)状态下, 定时执行, 建议300ms, 由该函数扫描周期和 speed_threshold 决定
+  * @note   未连接(离线)状态下, 定时执行, 建议 300ms, 由该函数扫描周期和 speed_threshold 决定
   * @param  handle: CMD_Network_Handle_t 类型
   * @retval CMD_Network_State_t: CMD_NETWORK_OK, CMD_NETWORK_ERR_TIMEOUT
   */
 CMD_Network_State_t CMD_Network_Task(CMD_Network_Handle_t handle);
 
 
+
 /**
   * @brief  网络指令添加(最多 CMD_NETWORK_MENU_MEMPOOL_ELEMENT_COUNT 条)范围: 指令数量 < CMD_NETWORK_MENU_MEMPOOL_ELEMENT_COUNT
   * @note   使用方法: CMD_Network_Directive_Add(handle, TT_CSQ, TT_Send_CSQ, TT_Recv_CSQ);
   * @param  handle: CMD_Core_Handle_t 类型
-  * @param  directive_index: 网络指令的唯一标识
-  * @param  send_func: 	联网指令唯一标识对应的发送函数,例如 CSQ 那么该函数就是实现 AT+CSQ 指令
-  * @param  check_func: 联网指令唯一标识对应的校验函数,例如 CSQ 那么该函数就是校验 AT+CSQ 指令模块返回的数据
+  * @param  directive_logo: 网络指令的唯一标识, 可以是 0 - uint32_t 中的任何值
+  * @param  send_func: 	联网指令唯一标识对应的发送函数, 例如 CSQ 那么该函数就是实现 AT+CSQ 指令
+  * @param  check_func: 联网指令唯一标识对应的校验函数
+							char *frame 是每帧数据的起始地址指针, 用户可以用来校验模块返回的数据是否正确
+							char * 返回值要求: 用户校验成功返回 !NULL, 校验失败返回 NULL
   * @retval CMD_Network_State_t: CMD_NETWORK_OK, CMD_NETWORK_ERR_PARAM, CMD_NETWORK_ERR_REPEAT, CMD_NETWORK_STATE_FULL
   */
-CMD_Network_State_t CMD_Network_Directive_Add(CMD_Network_Handle_t handle, uint32_t directive_index, void(*send_func)(void), char *(check_func)(void));
+CMD_Network_State_t CMD_Network_Directive_Add(CMD_Network_Handle_t handle, uint32_t directive_logo, void(*send_func)(void), char *(*check_func)(char *frame));
+
+
 
 /**
   * @brief  网络指令删除
   * @note   使用方法: CMD_Network_Directive_Delete(handle, TT_CSQ);
   * @param  handle: CMD_Core_Handle_t 类型
-  * @param  directive_index: 网络指令的唯一标识
+  * @param  directive_logo: 网络指令的唯一标识
   * @retval CMD_Network_State_t: CMD_NETWORK_OK, CMD_NETWORK_ERR_PARAM, CMD_NETWORK_ERR_INDEX
   */
-CMD_Network_State_t CMD_Network_Directive_Delete(CMD_Network_Handle_t handle, uint32_t directive_index);
+CMD_Network_State_t CMD_Network_Directive_Delete(CMD_Network_Handle_t handle, uint32_t directive_logo);
 
 
 
 /**
-  * @brief  网络连接起始指令设置
+  * @brief  网络起始连接指令设置
   * @note   每个网络指令在添加的时候都会有个唯一标识号
   * @param  handle: CMD_Core_Handle_t 类型
 	* @param  start: 网络起始连接指令,唯一标识
@@ -103,11 +115,13 @@ CMD_Network_State_t CMD_Network_Directive_Delete(CMD_Network_Handle_t handle, ui
   */
 CMD_Network_State_t CMD_Network_StartLinkDirective_Set(CMD_Network_Handle_t handle, uint32_t start);
 
+
+
 /**
-  * @brief 	网络连接当前指令设置
-  * @note   用户可以在接收发送AT命令函数内自行修改当前联网步骤
+  * @brief 	网络当前连接指令设置
+  * @note   用户可以在校验命令函数内自行修改当前联网步骤
   * @param  handle: CMD_Core_Handle_t 类型
-  * @param  current: 当前连接的起始指令,唯一标识
+  * @param  current: 网络当前连接指令,唯一标识
   * @retval CMD_Network_State_t: CMD_NETWORK_OK, CMD_NETWORK_ERR_INDEX
   */
 CMD_Network_State_t CMD_Network_CurrentLinkDirective_Set(CMD_Network_Handle_t handle, uint32_t current);
